ebf9d51ff26f31e7afa377662bfd2303da7a58f4
Alonso Lopez <alonso.lmdev@gmail.com>
2023-08-02 16:52:33 -0600

    Creation of the  shipment model with tests and validations

---
+++ shipments_tracker/test/models/shipment_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class ShipmentTest < ActiveSupport::TestCase
+  def setup
+    @account = Account.create(company_name: 'test', active: true)
+    @shipment = Shipment.new(
+      account_id: @account.id,
+      status: "registered",
+      tracking_history: ["Event 1"],
+      tracking_number: "ABC123"
+    )
+  end
+
+  test "should be valid" do
+    assert @shipment.valid?, @shipment.errors.full_messages.join(", ")
+  end
+
+  test "should require account_id" do
+    @shipment.account_id = nil
+    assert_not @shipment.valid?, @shipment.errors.full_messages.join(", ")
+  end
+
+  test "should require status" do
+    @shipment.status = nil
+    assert_not @shipment.valid?, @shipment.errors.full_messages.join(", ")
+  end
+end
