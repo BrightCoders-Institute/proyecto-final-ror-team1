8226d063cc2148a2a252374db94ebaef4fe7ac23
Manz1999 <adri4n1999@gmail.com>
2023-08-03 17:49:00 -0600

    indentation corrected

--- shipments_tracker/app/models/user.rb
+++ shipments_tracker/app/models/user.rb
@@ -1,18 +1,18 @@
 class User < ApplicationRecord
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable,
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
 
-    # Ensures that the account_id field is present (not empty).
-    validates :id, presence: true
+  # Ensures that the account_id field is present (not empty).
+  validates :id, presence: true
 
-    # Ensures that the email field is present, unique, and has a valid email address format.
-    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+  # Ensures that the email field is present, unique, and has a valid email address format.
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
 
-    # Ensures that the role field is present and only accepts the values 'admin', 'owner', or 'staff'.
-    validates :role, presence: true, inclusion: { in: %w(admin owner staff) }
+  # Ensures that the role field is present and only accepts the values 'admin', 'owner', or 'staff'.
+  validates :role, presence: true, inclusion: { in: %w(admin owner staff) }
 
-    # Ensures that the account_id field is present (not empty).
-    validates :account_id, presence: true
+  # Ensures that the account_id field is present (not empty).
+  validates :account_id, presence: true
 end
