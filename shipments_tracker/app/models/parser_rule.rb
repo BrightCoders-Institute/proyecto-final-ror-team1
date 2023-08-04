9be079c93b6d6ec73acde9db9796414f0c940a32
Alonso Lopez <alonso.lmdev@gmail.com>
2023-08-04 17:55:01 -0600

    hotfix: parser rule model typo

--- shipments_tracker/app/models/parser_rule.rb
+++ shipments_tracker/app/models/parser_rule.rb
@@ -2,7 +2,7 @@ class ParserRule < ApplicationRecord
   validates :code, presence: true, length: { maximum: 50 }
   validates :description, presence: true
   validates :carrier_id, presence: true, numericality: { only_integer: true }
-  validates :internal_code, presence: true, uniqueness: true d
+  validates :internal_code, presence: true, uniqueness: true
 
   belongs_to :carrier
 end
