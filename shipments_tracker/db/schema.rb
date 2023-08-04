0e88d1666bebb37fc126e16943b9d6e53e7ac81f
VicAb Garcia <victor.ag71@gmail.com>
2023-08-04 16:04:30 -0600

    Change settings column to jsonb

--- shipments_tracker/db/schema.rb
+++ shipments_tracker/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_04_000126) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_04_214644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_04_000126) do
   create_table "carriers", force: :cascade do |t|
     t.string "name", null: false
     t.string "url", null: false
-    t.string "settings"
+    t.jsonb "settings"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
