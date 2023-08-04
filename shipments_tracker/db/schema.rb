b60136a72377edab05545f701295505d62d555c3
Alonso Lopez <alonso.lmdev@gmail.com>
2023-08-04 17:58:43 -0600

    rebase from development

--- shipments_tracker/db/schema.rb
+++ shipments_tracker/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_03_025534) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_04_214644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_03_025534) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "carriers", force: :cascade do |t|
     t.string "name", null: false
